b51bd1b0b4c87679a4e02d7a4fbd9f78adee77f7
jpelletier1 <44589723+jpelletier1@users.noreply.github.com>
2024-02-15 15:41:22 -0500

    new ec2 type

--- custom_check.tf
+++ custom_check.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "foo" {
   ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "m7i-flex.large"
+  instance_type = "m7g.metal"
 
   network_interface {
     network_interface_id = aws_network_interface.foo.id
